2e45d6e64e70ced613d720f73b6d278fa39a96e1
Eric Churchill <eric.churchill2@gmail.com>
2016-12-15 13:35:31 -0800

    [chore] Update .eslint to work with flowtype

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
 
-  "extends": ["eslint:recommended"],
+  "extends": [
+    "eslint:recommended",
+    "plugin:flowtype/recommended"
+  ],
 
   "parserOptions": {
     "ecmaVersion": 6,
