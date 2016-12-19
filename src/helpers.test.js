4b219a0a6b4e28ffd7e1fabf92983268f7a43a86
Eric Churchill <eric.churchill2@gmail.com>
2016-12-19 00:23:49 -0800

    test(helpers): update readPackageJson tests

--- src/helpers.test.js
+++ src/helpers.test.js
@@ -1,26 +1,41 @@
-import { expect } from 'chai';
+import chai, { expect, beforeEach } from 'chai';
+import chaiSpies from 'chai-spies';
 import { NpmConfig, readPackageJson } from './helpers';
 import { getNpmData } from './ramda';
+
+chai.use(chaiSpies);
 
 describe('Helpers', function() {
 
   describe('readPackageJson', () => {
-    it('should be a function', () => {
+
+    it('should be a function that accepts one argument and one optional argument', () => {
       expect(readPackageJson).to.be.a('function');
+      expect(readPackageJson.length).to.equal(1);
     });
+
     it('should throw TypeError when passed argument that is not a function', () => {
       expect(readPackageJson).to.throw(TypeError);
       expect(() => readPackageJson(42)).to.throw(TypeError);
       expect(() => readPackageJson('bad input')).to.throw(TypeError);
     });
+
+    it('should throw TypeError when passed argument that is not a string', () => {
+      expect(() => readPackageJson(() => {}, 42)).to.throw(TypeError);
+    });
+
     it('should return a promise object when passed callback', () => {
       expect(readPackageJson(() => {})).to.be.an.instanceof(Promise);
     });
-    it('should throw error when path does not exist', () => {
-      // TODO:
-      expect().to.be.ok;
+
+    it('should throw error when path does not exist', (done) => {
+      readPackageJson(() => {}, '').catch((reason) => {
+        expect(reason).to.be.an.instanceof(Error);
+        done();
+      });
     });
-    it('should return project\'s parsed package.json', () => {
+
+    it('should return project\'s parsed package.json', (done) => {
       readPackageJson((next) => next).then((pkg) => {
         expect(pkg).to.be.an('object');
         expect(pkg).to.contain.all.keys('name', 'description');
@@ -33,8 +48,21 @@ describe('Helpers', function() {
           'dependencies',
           'devDependencies'
         );
+        done();
       });
     });
+
+    // SPY on basic function
+    const spy = chai.spy(() => {});
+
+    it('should invoke callback function', (done) => {
+      expect(spy).to.be.spy;
+      readPackageJson(spy).then(() => {
+        expect(spy).to.have.been.called.at.least(1);
+        done();
+      });
+    });
+
   });
 
   const deps = {
