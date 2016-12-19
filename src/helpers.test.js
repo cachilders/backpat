890cd7afa258a3c60f31fd404c86f46b76aafb0b
Eric Churchill <eric.churchill2@gmail.com>
2016-12-18 21:50:09 -0800

    test(helpers): wrote asynchronous test for getNpmData

--- src/helpers.test.js
+++ src/helpers.test.js
@@ -1,12 +1,15 @@
 import { expect } from 'chai';
 import { NpmConfig } from './helpers';
+import { getNpmData } from './ramda';
 
 describe('Helpers', function() {
+
+  const deps = {
+    "lodash": "4.17.2",
+    "ramda": "0.22.1"
+  };
+
   describe('NpmConfig', () => {
-    const deps = {
-        "lodash": "4.17.2",
-        "ramda": "0.22.1"
-    };
     it('should be a factory that returns a config object', () => {
       expect(NpmConfig).to.be.a('function');
       const obj = NpmConfig(deps);
@@ -21,4 +24,24 @@ describe('Helpers', function() {
       });
     });
   });
+
+  describe('getNpmData', () => {
+    it('should be a function', () => {
+      expect(getNpmData).to.be.a('function');
+    });
+    it('should return a promise object', () => {
+      expect(getNpmData(deps)).to.be.an.instanceof(Promise);
+    });
+    it('should make an https GET request', (done) => {
+      // const config = NpmConfig(deps);
+      getNpmData(deps).then( (result) => {
+        const response = JSON.parse(result);
+        expect(response).to.be.an('object');
+        expect(response.lodash).to.have.all.keys('downloads', 'start', 'end', 'package');
+        expect(response.ramda).to.have.all.keys('downloads', 'start', 'end', 'package');
+        done();
+      });
+    });
+  });
+
 });
