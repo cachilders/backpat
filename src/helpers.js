165de94494103887afdf097e4acefe9014bbdb1b
Eric Churchill <eric.churchill2@gmail.com>
2016-12-18 20:17:49 -0800

    refactor(helpers): rewrite helper functions to improve functional purity

--- src/helpers.js
+++ src/helpers.js
@@ -1,72 +1,64 @@
 /* @flow */
 
-var _ = require('ramda');
 var https = require('https');
-var fs    = require('fs');
+// var fs    = require('fs');
+//
+// var vars  = require('./index');
+//
+// import { formatVersionsAndFilterPrivate } from '';
 
-var vars  = require('./index');
-
-const isNotPrivate = _.compose(_.not, _.prop('private'));
-const filterPrivate = _.filter(isNotPrivate);
-const removeCaret = _.replace(/\^/, '');
-// TODO: RENAME something more expressive and less awkward
-const formatVersionsAndFilterPrivate = _.compose(_.map(removeCaret), filterPrivate);
-
-module.exports = {
-  // Add modules and their versions to the dependencies object
-  seedDependencies: function(obj: {}) {
-    vars.dependencies = formatVersionsAndFilterPrivate(obj);
-    module.exports.fetchModuleDownloads();
-  },
-
-  // Fetch each module's package.json and add details from it
-  gatherDetails: function(module) {
-    var modulePath = vars.rootDir + 'node_modules/' + module + '/package.json';
-    var dependency = vars.dependencies[module];
-    fs.readFile(modulePath, function(err, data) {
-      if (err) console.error(err);
-      var details = JSON.parse(data.toString('utf8'));
-      dependency.name = details.name;
-      dependency.url = details.homepage || details.repository ?
-        details.repository.url.replace(/\w*\+/, '') : '' ;
-      dependency.description = details.description;
-    });
-  },
+// module.exports = {
+//   // Add modules and their versions to the dependencies object
+//   seedDependencies: function(obj: {}) {
+//     vars.dependencies = formatVersionsAndFilterPrivate(obj);
+//     module.exports.fetchModuleDownloads();
+//   },
+//
+//   // Fetch each module's package.json and add details from it
+//   gatherDetails: function(module: string) {
+//     var modulePath = vars.rootDir + 'node_modules/' + module + '/package.json';
+//     var dependency = vars.dependencies[module];
+//     fs.readFile(modulePath, function(err, data) {
+//       if (err) console.error(err);
+//       var details = JSON.parse(data.toString('utf8'));
+//       dependency.name = details.name;
+//       dependency.url = details.homepage || details.repository ?
+//         details.repository.url.replace(/\w*\+/, '') : '' ;
+//       dependency.description = details.description;
+//     });
+//   },
 
   // Batch retrieve the npm download counts for all modules for the past month
-  fetchModuleDownloads: function() {
-    var httpOptions = {
-      hostname: 'api.npmjs.org',
-      path: '/downloads/point/last-month/' + Object.keys(vars.dependencies).join(','),
-      method: 'GET',
-      headers: {}
-    };
 
-    httpOptions.headers['User-Agent'] = 'cachilders/backpat';
-    var result = '';
-    https.get(httpOptions, function(res) {
-      res.on('data', function(data) {
-        result += data;
-      });
-    }).on('error', function(err) {
-      console.error(err);
-    }).on('close', function() {
-      var resultObj = JSON.parse(result.toString('utf8'));
-      var inCount = Object.keys(resultObj).length;
-      var outCount = 0;
-      Object.keys(resultObj).forEach(function(key) {
+// httpsGetPromise.then((result) => {
+//   var resultObj = JSON.parse(result.toString('utf8'));
+//   Object.keys(resultObj).forEach(function(key) {
+//     if (vars.dependencies[key]) {
+//       vars.dependencies[key].downloads = resultObj[key] ?
+//       resultObj[key].downloads : null;
+//       outCount++;
+//     }
+//   });
+// })
 
-      if (vars.dependencies[key]) {
-        vars.dependencies[key].downloads = resultObj[key] ?
-          resultObj[key].downloads : null;
-        outCount++;
-      }
-      });
+export function NpmConfig(dependencies: {}) {
+  return {
+    hostname: 'api.npmjs.org',
+    path: '/downloads/point/last-month/' + Object.keys(dependencies).join(','),
+    method: 'GET',
+    headers: {
+      'User-Agent': 'cachilders/backpat'
+    }
+  };
+}
 
-      if (inCount === outCount) {
-        vars.event.emit('gathered');
-      }
-
+export function httpsGetPromise(opts: {}) {
+  return new Promise((resolve, reject) => {
+    https.get(opts, (res) => {
+      const body = [];
+      res.on('data', (chunk) => body.push(chunk));
+      res.on('end', () => resolve(Buffer.concat(body).toString()));
+      res.on('error', reject);
     });
-  }
-};
+  });
+}
---
+++ src/index_REMOTE_17894.js
@@ -0,0 +1,44 @@
+/* @flow */
+
+var fs           = require('fs');
+var EventEmitter = require('events');
+var helpers      = require('./helpers');
+
+exports.rootDir = process.cwd() + '/';
+exports.dependencies = {};
+exports.event = new EventEmitter();
+
+exports.backpat = function(callback: Function) {
+  if (typeof callback !== 'function') {
+    throw new TypeError('backpat should accept type function as input parameter');
+  }
+  fs.readFile(exports.rootDir + '/package.json', function(err, data) {
+    if (err) throw err;
+    var pkgjsn = JSON.parse(data.toString('utf8'));
+
+    if (pkgjsn.dependencies) {
+      helpers.seedDependencies(pkgjsn.dependencies);
+    }
+
+    if (pkgjsn.devDependencies) {
+      helpers.seedDependencies(pkgjsn.devDependencies);
+    }
+
+    for (var k in exports.dependencies) {
+      helpers.gatherDetails(k);
+    }
+
+    // Manually inject Node because it's certainly part of your stack
+    exports.dependencies.node = {
+      name        : 'Node.js',
+      version     : process.versions.node,
+      description : 'A JavaScript runtime ✨🐢🚀✨',
+      downloads   : 10000000 // A fake number since Node isn't downloaded on npm
+    };
+
+    exports.event.on('complete', function() {
+      return callback(exports.dependencies);
+    });
+
+  });
+};
