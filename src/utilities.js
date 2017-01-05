6289585ffbeeb5911b0f8361e3829a7bcfdf8dac
Casey Childers <cachilders@gmail.com>
2017-01-05 13:44:25 -0800

    refactor: Remove extraneous curry operation

--- src/utilities.js
+++ src/utilities.js
@@ -5,10 +5,9 @@ const removeCaret = R.replace(/\^/, '');
 const addVersionProp = (v, k, o) => o[k] = { 'version': removeCaret(v) };
 const isNotPrivate = R.compose(R.not, R.prop('private'));
 const deeplyMerge = (obj1, obj2) => R.mapObjIndexed((v, k, o) => R.merge(obj1[k], obj2[k]), obj1);
-const pickProps = R.curry(R.pick);
 
 export const formatVersions = R.mapObjIndexed(addVersionProp);
 export const curriedMerge = R.curry(deeplyMerge);
 export const getNpmData = R.compose(httpsGetPromise, NpmConfig);
-export const pickDownloads = R.map(pickProps(['downloads']));
+export const pickDownloads = R.map(R.pick(['downloads']));
 export const filterPrivate = R.filter(isNotPrivate);
