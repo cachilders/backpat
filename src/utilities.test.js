a5cd9e7be82a69cc245f1bd854d5ac708b458e77
Casey Childers <cachilders@gmail.com>
2017-01-04 21:16:29 -0800

    test(utilities): Write initial tests for new functions

--- src/utilities.test.js
+++ src/utilities.test.js
@@ -3,8 +3,33 @@ import {
   pickDownloads,
   curriedMerge,
   formatVersionsAndFilterPrivate,
-  getNpmData,
   addNode
 } from './utilities';
 
-// TODO
+describe('Utilities', () => {
+
+  describe('pickDownloads', () => {
+    it('should be a function', () => {
+      expect(pickDownloads).to.be.a('function');
+    });
+  });
+
+  describe('curriedMerge', () => {
+    it('should be a function', () => {
+      expect(curriedMerge).to.be.a('function');
+    });
+  });
+
+  describe('formatVersionsAndFilterPrivate', () => {
+    it('should be a function', () => {
+      expect(formatVersionsAndFilterPrivate).to.be.a('function');
+    });
+  });
+
+  describe('addNode', () => {
+    it('should be a function', () => {
+      expect(addNode).to.be.a('function');
+    });
+  });
+
+});
